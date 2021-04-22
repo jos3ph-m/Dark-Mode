7ccad28326535fda8622f4688ac5593cb155dead
Joseph M <jmedina1985@hotmail.com>
2021-04-21 21:53:06 -0700

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return <main>dark mode starter</main>;
 }
 
-export default App
+export default App;
