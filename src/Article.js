f2f24057a268c204b09f02fee7efe0fc8542304f
Joseph M <jmedina1985@hotmail.com>
2021-04-21 21:56:28 -0700

    Getting started

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from 'react';
+import moment from 'moment';
+const Article = ({ title, snippet, date, length }) => {
+  return <h2>article component</h2>;
+};
 
-export default Article
+export default Article;
