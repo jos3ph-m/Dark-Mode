74f5ea1a2af4c81397f1aeca8c803d29cb93d66d
Joseph M <jmedina1985@hotmail.com>
2021-04-21 21:56:42 -0700

    Getting started

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 const Article = ({ title, snippet, date, length }) => {
-  return <h2>article component</h2>;
+  return <article className="post">article component</article>;
 };
 
 export default Article;
