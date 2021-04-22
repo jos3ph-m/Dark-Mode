a032be6ff801cd8520ae0553b097d7febc5650c5
Joseph M <jmedina1985@hotmail.com>
2021-04-21 21:57:42 -0700

    Getting started

--- src/Article.js
+++ src/Article.js
@@ -4,7 +4,10 @@ const Article = ({ title, snippet, date, length }) => {
   return (
     <article className="post">
       <h2>{title}</h2>
-      <div className="post-info"></div>
+      <div className="post-info">
+        <span>date</span>
+        <span>{length} min read</span>
+      </div>
     </article>
   );
 };
