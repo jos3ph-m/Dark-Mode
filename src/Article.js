fb0ccf85c712c93f09f68712484837dcc102d9df
Joseph M <jmedina1985@hotmail.com>
2021-04-22 21:03:05 -0700

    Article started up

--- src/Article.js
+++ src/Article.js
@@ -5,7 +5,7 @@ const Article = ({ title, snippet, date, length }) => {
     <article className="post">
       <h2>{title}</h2>
       <div className="post-info">
-        <span>{date}</span>
+        <span>date</span>
         <span>{length} min read</span>
       </div>
       <p>{snippet}</p>
